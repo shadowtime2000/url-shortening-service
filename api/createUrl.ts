2ab20133034310996927bee17fef7757a68d83ab
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-24 15:28:00 -0700

    changes

--- api/createUrl.ts
+++ api/createUrl.ts
@@ -1,9 +1,9 @@
 import urlDb from "../_urlDb";
-import clicksDb from '../_clicksDb'
+import clicksDb from "../_clicksDb";
 
 export = async (req, res) => {
   let code = Math.random().toString(36).slice(2);
   await urlDb.set(code, req.query.url);
-  await clicksDb.set(code, "0")
+  await clicksDb.set(code, "0");
   res.send(JSON.stringify({ id: code, url: req.query.url }));
 };
--- api/url.ts
+++ api/url.ts
@@ -1,9 +1,14 @@
 import urlDb from "../_urlDb";
+import clicksDb from "../_clicksDb";
 
 export = async (req, res) => {
   if (await urlDb.has(req.query.id)) {
     res.send(
-      JSON.stringify({ id: req.query.id, url: await urlDb.get(req.query.id) })
+      JSON.stringify({
+        id: req.query.id,
+        url: await urlDb.get(req.query.id),
+        clicks: await clicksDb.get(req.query.id),
+      })
     );
   } else {
     res.status(422).send();
