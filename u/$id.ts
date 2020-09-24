d2cac0534b61616db504fe4efa2ea1ef2b4f2c02
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-24 13:21:44 -0700

    Link Click Logging

--- u/$id.ts
+++ u/$id.ts
@@ -2,6 +2,7 @@ import clicksDb from '../_clicksDb'
 
 export = async (req, res) => {
     await clicksDb.set(req.params.id, ( await clicksDb.get(req.params.id) + 1 || 1))
+    console.log(`${req.params.id} clicked`)
     fetch(`/api/url?id=${req.params.id}`)
         .then(resp => resp.json())
         .then(async resp => {
