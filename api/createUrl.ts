6a103eba12180e12d7f3eaf14dd8c9322863e920
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-24 13:16:51 -0700

    Rename

--- api/createUrl.ts
+++ api/createUrl.ts
@@ -1,7 +1,7 @@
-import endb from '../_urlDb'
+import urlDb from '../_urlDb'
 
 export =  async (req, res) => {
     let code = Math.random().toString(36).slice(2)
-    await endb.set(code, req.query.url)
+    await urlDb.set(code, req.query.url)
     res.send(JSON.stringify({ id: code, url: req.query.url }))
 }
--- api/url.ts
+++ api/url.ts
@@ -1,8 +1,8 @@
-import endb from '../_urlDb'
+import urlDb from '../_urlDb'
 
 export = async (req, res) => {
-    if ( await endb.has(req.query.id)) {
-        res.send(JSON.stringify({ id: req.query.id, url: await endb.get(req.query.id)}))
+    if ( await urlDb.has(req.query.id)) {
+        res.send(JSON.stringify({ id: req.query.id, url: await urlDb.get(req.query.id)}))
     } else {
         res.status(422).send()
     }
