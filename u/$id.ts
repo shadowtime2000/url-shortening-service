a4509b0326e155dd2bcba609b3e3e816082bb843
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-24 13:11:07 -0700

    Logging Time

--- u/$id.ts
+++ u/$id.ts
@@ -1,6 +1,12 @@
+import { Expression } from "typescript"
 
 export = async (req, res) => {
     fetch(`/api/url?id=${req.params.id}`)
         .then(resp => resp.json())
-        .then(resp => res.redirect(resp.url))
+        .then(async resp => {
+            const time1 = new Date().getMilliseconds()
+            await res.redirect(resp.url)
+            const time2 = new Date().getMilliseconds()
+            console.log(`Time taken: ${time2 - time1}`)
+        })
 }
